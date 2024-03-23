9fb97855fdeb88fd7d69a5c858ebe07fd62d5b4e
Joakim Isaksson <joakim.isaksson@ha.ax>
2024-03-23 14:19:51 +0200

    Create CountableProvider

--- App.js
+++ App.js
@@ -1,52 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import { useState, useEffect } from "react";
 import { StyleSheet } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 import { CountablesView } from "./components/CountablesView";
-import { CountableContext } from "./providers/CountableProvider";
-import { loadCountables, saveCountables } from "./storage/CountableStorage";
+import { CountableProvider } from "./providers/CountableProvider";
 
 export default function App() {
-  const [countables, setCountables] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    loadCountables().then((result) => {
-      setCountables(result);
-      setIsLoaded(true);
-    });
-  }, []);
-
-  useEffect(() => {
-    if (isLoaded) {
-      saveCountables(countables);
-    }
-  }, [countables, isLoaded]);
-
-  const changeCount = (amount, index) => {
-    const newState = [...countables];
-    newState[index].count += amount;
-    setCountables(newState);
-    saveCountables(newState);
-  };
-
-  const addNewCountable = (name) => {
-    const newState = [...countables, { name, count: 0 }];
-    setCountables(newState);
-    saveCountables(newState);
-  };
-
-  // https://medium.com/@nickyang0501/keyboardavoidingview-not-working-properly-c413c0a200d4
-
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <CountableContext.Provider
-          value={{ countables, changeCount, addNewCountable }}
-        >
+        <CountableProvider>
           <CountablesView />
-        </CountableContext.Provider>
+        </CountableProvider>
         <StatusBar style="auto" />
       </SafeAreaView>
     </SafeAreaProvider>
--- providers/CountableProvider.js
+++ providers/CountableProvider.js
@@ -1,3 +1,44 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
+
+import { loadCountables, saveCountables } from "../storage/CountableStorage";
 
 export const CountableContext = createContext({});
+
+export const CountableProvider = ({ children }) => {
+  const [countables, setCountables] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    loadCountables().then((result) => {
+      setCountables(result);
+      setIsLoaded(true);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (isLoaded) {
+      saveCountables(countables);
+    }
+  }, [countables, isLoaded]);
+
+  const changeCount = (amount, index) => {
+    const newState = [...countables];
+    newState[index].count += amount;
+    setCountables(newState);
+    saveCountables(newState);
+  };
+
+  const addNewCountable = (name) => {
+    const newState = [...countables, { name, count: 0 }];
+    setCountables(newState);
+    saveCountables(newState);
+  };
+
+  return (
+    <CountableContext.Provider
+      value={{ countables, changeCount, addNewCountable }}
+    >
+      {children}
+    </CountableContext.Provider>
+  );
+};
