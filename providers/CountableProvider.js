0d96b7e34e3a6e708f2ef51120d19745ab3ed29a
Joakim Isaksson <joakim.isaksson@ha.ax>
2024-03-23 15:23:26 +0200

    Using a reducer in the provider

--- providers/CountableProvider.js
+++ providers/CountableProvider.js
@@ -1,16 +1,38 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useReducer } from "react";
 
 import { loadCountables, saveCountables } from "../storage/CountableStorage";
 
 export const CountableContext = createContext({});
 
 export const CountableProvider = ({ children }) => {
-  const [countables, setCountables] = useState([]);
+  const countablesReducer = (countables, action) => {
+    switch (action.type) {
+      case "addedNew": {
+        return [...countables, { name: action.name, count: 0 }];
+      }
+      case "changedCount": {
+        const newState = [...countables];
+        newState[action.index].count += action.amount;
+        return newState;
+      }
+      case "loaded": {
+        return action.countables;
+      }
+      default: {
+        throw Error("Unknown action: " + action.type);
+      }
+    }
+  };
+
+  const [countables, dispatch] = useReducer(countablesReducer, []);
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     loadCountables().then((result) => {
-      setCountables(result);
+      dispatch({
+        type: "loaded",
+        countables: result,
+      });
       setIsLoaded(true);
     });
   }, []);
@@ -22,14 +44,18 @@ export const CountableProvider = ({ children }) => {
   }, [countables, isLoaded]);
 
   const changeCount = (amount, index) => {
-    const newState = [...countables];
-    newState[index].count += amount;
-    setCountables(newState);
+    dispatch({
+      type: "changedCount",
+      amount,
+      index,
+    });
   };
 
   const addNewCountable = (name) => {
-    const newState = [...countables, { name, count: 0 }];
-    setCountables(newState);
+    dispatch({
+      type: "addedNew",
+      name,
+    });
   };
 
   return (
