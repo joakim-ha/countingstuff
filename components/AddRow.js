56773e614183991ee90e82b360ec1ece0a26890c
Casper Söderström <casper.soderstrom@ha.ax>
2024-04-02 13:57:01 +0300

    Added improvements

--- components/AddRow.js
+++ components/AddRow.js
@@ -1,21 +1,44 @@
 import { useState } from "react";
-import { View, TextInput } from "react-native";
+import { View, TextInput, StyleSheet, Keyboard } from "react-native";
 
 import { CountableButton } from "./CountableButton";
 import { CommonStyles } from "../styles/CommonStyles";
 
-export const AddRow = ({ addNewCountable }) => {
+export const AddRow = ({ addNewCountable, countables }) => {
   const [name, setName] = useState("");
 
+  const handleAddCountable = () => {
+    let nameExists = false;
+    for (let i = 0; i < countables.length; i++) {
+      if (countables[i].name === name) {
+        nameExists = true;
+        break;
+      }
+    }
+    if (!nameExists && name.trim() !== "") {
+      addNewCountable(name);
+      setName("");
+      Keyboard.dismiss();
+    }
+  };
   return (
     <View style={CommonStyles.row}>
-      <TextInput placeholder="Enter name" onChangeText={setName} />
-      <CountableButton
-        label="Add"
-        submit={() => {
-          addNewCountable(name);
-        }}
+      <TextInput
+        style={styles.textColumn}
+        placeholder="Enter name"
+        onChangeText={setName}
+        value={name}
       />
+      <CountableButton label="Add" submit={handleAddCountable} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  addColumn: {
+    flex: 0.1,
+  },
+  textColumn: {
+    flex: 0.9,
+  },
+});
