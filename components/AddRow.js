b24444ad92b260f5e9664a34d8415ba559a22839
Lem0on <gabriel.persson@ha.ax>
2024-04-02 21:09:05 +0300

    improvements

--- components/AddRow.js
+++ components/AddRow.js
@@ -1,21 +1,65 @@
-import { useState } from "react";
-import { View, TextInput } from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, StyleSheet, Keyboard } from "react-native";
 
 import { CountableButton } from "./CountableButton";
 import { CommonStyles } from "../styles/CommonStyles";
 
-export const AddRow = ({ addNewCountable }) => {
+export const AddRow = ({ addNewCountable, existingNames }) => {
   const [name, setName] = useState("");
+  const [isNameEmpty, setIsNameEmpty] = useState(true); // State to track if name is empty
+
+  const handleNameChange = (text) => {
+    setName(text);
+    setIsNameEmpty(text.trim() === ""); // Update isNameEmpty based on text input
+  };
+
+  const handleAddCountable = () => {
+    if (!existingNames.includes(name)) {
+      addNewCountable(name);
+      setName(""); // Clear input after adding
+      setIsNameEmpty(true); // Reset isNameEmpty state after adding
+      Keyboard.dismiss(); // Close the keyboard
+    } else {
+      alert("A countable with the same name already exists!");
+    }
+  };
 
   return (
-    <View style={CommonStyles.row}>
-      <TextInput placeholder="Enter name" onChangeText={setName} />
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter name"
+        onChangeText={handleNameChange} // Pass the handler function
+        value={name}
+      />
       <CountableButton
         label="Add"
-        submit={() => {
-          addNewCountable(name);
-        }}
+        submit={handleAddCountable}
+        style={styles.addButton}
+        disabled={isNameEmpty} // Disable the button when the name is empty
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  input: {
+    flex: 1,
+    marginRight: 10,
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+  },
+  addButton: {
+    marginLeft: 10,
+  },
+});
