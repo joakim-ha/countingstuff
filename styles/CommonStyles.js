e2fbe8d4c08892c2c774fe43d6f0101f1b9ee60f
Daniel Hannus <prozeyin.co@gmail.com>
2024-04-03 18:09:29 +0300

    Add styles for the add button and text input

--- styles/CommonStyles.js
+++ styles/CommonStyles.js
@@ -14,4 +14,29 @@ export const CommonStyles = StyleSheet.create({
     paddingEnd: 20,
     paddingStart: 20,
   },
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingLeft: 10,
+    paddingRight: 10,
+    fontSize: 18,
+    flexGrow: 1, //this makes the text input expand to fill available space
+    marginVertical: 10,
+    marginRight: 15,
+  },
+  addButton: {
+    backgroundColor: '#007BFF',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginVertical: 10,
+    alignSelf: 'flex-start', // positions the button to the start of the flex container
+  },
+  addButtonText: {
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 20,
+  },
 });
