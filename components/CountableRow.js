fd6cd832613a00020d269a2a8cf23c608171ac5d
Daniel Hannus <prozeyin.co@gmail.com>
2024-04-03 18:07:39 +0300

    Add Enable Deletion functionality using item name

--- components/CountableRow.js
+++ components/CountableRow.js
@@ -1,31 +1,43 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 import { CountableButton } from "./CountableButton";
 import { CommonStyles } from "../styles/CommonStyles";
 
-export const CountableRow = ({ countable, changeCount, index }) => (
-  <View style={CommonStyles.row}>
-    <View style={styles.nameColumn}>
-      <Text style={CommonStyles.textItem}>{countable.name}</Text>
-      <Text style={CommonStyles.textItem}>{countable.count}</Text>
+export const CountableRow = ({ countable, changeCount, index, onDelete }) => {
+  /*
+  console.log(`Count for ${countable.name}:`, countable.count);
+  console.log(
+    `Is '-' button disabled for ${countable.name}?`,
+    countable.count < 1
+  );
+  */
+  return (
+    <View style={CommonStyles.row}>
+      <View style={styles.nameColumn}>
+        <Text style={CommonStyles.textItem}>{countable.name}</Text>
+        <Text style={CommonStyles.textItem}>{countable.count}</Text>
+      </View>
+      <View style={styles.buttonColumn}>
+        <CountableButton
+          label="+"
+          submit={() => {
+            changeCount(1, index);
+          }}
+        />
+        <CountableButton
+          label="-"
+          submit={() => {
+            changeCount(-1, index);
+          }}
+          disabled={countable.count < 1}
+        />
+        <TouchableOpacity onPress={() => onDelete()}>
+          <Text>Delete</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-    <View style={styles.buttonColumn}>
-      <CountableButton
-        label="+"
-        submit={() => {
-          changeCount(1, index);
-        }}
-      />
-      <CountableButton
-        label="-"
-        submit={() => {
-          changeCount(-1, index);
-        }}
-      />
-    </View>
-  </View>
-);
-
+  );
+};
 const styles = StyleSheet.create({
   nameColumn: {
     flex: 0.8,
